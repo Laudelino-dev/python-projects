c8aabef9a17f0eb81560258904b57464bc1cb294
Laudelino-dev <dev.laudelino@gmail.com>
2026-09-02 06:48:43 -0300

    Implement multiplication table program
    The program prompts the user for a number and displays its multiplication table from 1 to 10.

---
+++ exercicios/OperadoresAritmeticos/ex09Tabuada.py
@@ -0,0 +1,27 @@
+# Desafio09
+# Faça um programa que leia um numero e faça sua tabuada na tela
+
+n = int(input(' Digite um numero para ver sua tabuada: '))
+a = n * 1
+b = n * 2 
+c = n * 3
+d = n * 4
+e = n * 5
+f = n * 6
+j = n * 7
+g = n * 8
+h = n * 9
+i = n * 10
+print('O valor que você digitou foi : {}'.format(n))
+print('-' * 12)
+print('{} x 1  = {}'.format(n, a))
+print('{} x 2  = {}'.format(n, b))
+print('{} x 3  = {}'.format(n, c))
+print('{} x 4  = {}'.format(n, d))
+print('{} x 5  = {}'.format(n, e))
+print('{} x 6  = {}'.format(n, f))
+print('{} x 7  = {}'.format(n, j))
+print('{} x 8  = {}'.format(n, g))
+print('{} x 9  = {}'.format(n, h))
+print('{} x 10 = {}'.format(n, i))
+print('-' * 12)
