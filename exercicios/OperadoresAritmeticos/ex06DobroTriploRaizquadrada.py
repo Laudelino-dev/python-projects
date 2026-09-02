eacb5e2d53ea7be8d1b6ba8052745f72b07fcef6
Laudelino-dev <dev.laudelino@gmail.com>
2026-09-02 06:46:45 -0300

    Add script to calculate double, triple, and square root
    This script reads a number and calculates its double, triple, and square root, displaying the results.

---
+++ exercicios/OperadoresAritmeticos/ex06DobroTriploRaizquadrada.py
@@ -0,0 +1,9 @@
+# Desafio 06
+# Leia um numero e mostre o seu dobro, triplo e sua raiz quadrada
+
+n1 = int(input('Digite um valor: '))
+d = n1 * 2
+t = n1 * 3
+rq = n1**(1/2)
+
+print(' O número que você escolheu é {}, \n o dobro dele é {}, \n o triplo dele é {} \n e a raiz quadrada dele é {}'.format(n1, d, t, rq))
