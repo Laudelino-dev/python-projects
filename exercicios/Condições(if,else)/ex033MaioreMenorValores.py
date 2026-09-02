b24e393851654d5dfaaa91059d3254c920595714
Laudelino-dev <dev.laudelino@gmail.com>
2026-09-02 07:02:49 -0300

    Add program to determine max and min of three numbers
    Implement program to find the largest and smallest of three numbers.

---
+++ exercicios/Condições(if,else)/ex033MaioreMenorValores.py
@@ -0,0 +1,7 @@
+# Faça umm programa que leia três numeros e mostre qual é o maior e qual é o menor.
+
+n1 = int(input('Digite o primeiro numero: '))
+n2 = int(input('Digite o segundo numero: '))
+n3 = int(input('Digite o terceiro numero: '))
+print('O seu maior numero é : {}'.format(max(n1, n2, n3)))
+print('O seu menor numero é : {}'.format(min(n1, n2, n3)))
